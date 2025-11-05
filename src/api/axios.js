805318e70630e10db368364f1cc6f80288ec03dd
ankit <ankity9790@gmail.com>
2025-11-05 20:53:34 +0530

    update it for deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://texteditorbackend-uxt9.onrender.com/api",
 });
 
 instance.interceptors.request.use((config) => {
