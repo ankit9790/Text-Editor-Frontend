c5256ad75b219c28576e25690b66748e30ff319c
ankit <ankity9790@gmail.com>
2025-11-26 11:43:59 +0530

    fix bug

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://texteditorbackend-uxt9.onrender.com",
   // baseURL: "https://texteditorbackend-uxt9.onrender.com/api",
   withCredentials: false,
 });
